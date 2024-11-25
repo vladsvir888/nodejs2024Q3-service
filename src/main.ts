9edd6d49c4274fcd4e5ea8e0b3a63b7e6f68a47d
vsvir <vladsvir888@gmail.com>
2024-11-25 22:16:38 +0300

    feat: add exception filter

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { CustomLogger } from './logger/logger.service';
+import { HttpExceptionFilter } from './exceptions/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     bufferLogs: true,
   });
-  app.useLogger(app.get(CustomLogger));
+  const logger = app.get(CustomLogger);
+  app.useLogger(logger);
+  app.useGlobalFilters(new HttpExceptionFilter(logger));
   const options = new DocumentBuilder()
     .setTitle('Home Library Service')
     .setDescription('Home music library service')
