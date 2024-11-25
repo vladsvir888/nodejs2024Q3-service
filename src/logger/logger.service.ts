160bf353e56836b07f6fc695a6cbf6a42ce18cc3
vsvir <vladsvir888@gmail.com>
2024-11-26 00:45:53 +0300

    feat: nax file size limit and log levels

--- src/logger/logger.service.ts
+++ src/logger/logger.service.ts
@@ -1,50 +1,79 @@
 import { LoggerService, Injectable } from '@nestjs/common';
 import { join } from 'path';
 import { mkdirSync } from 'fs';
-import { appendFile } from 'fs/promises';
-import { LevelLog, TLevelLog } from './types';
+import { appendFile, stat, rm } from 'fs/promises';
+import { LevelLog, TLevelLog, numericLevelLog } from './types';
 
 @Injectable()
 export class CustomLogger implements LoggerService {
   private logsDir: string;
-  private logsFile: string;
-  private logsErrorFile: string;
+  private logLevel: number;
+  private maxFileSize: number;
+  private pathToLogsFile: string;
+  private pathToLogsErrorFile: string;
 
   constructor() {
+    const logsFile = 'logs.log';
+    const logsErrorFile = 'logs-error.log';
+    const currentWorkingDir = process.cwd();
     this.logsDir = 'logs';
-    this.logsFile = 'logs.log';
-    this.logsErrorFile = 'logs-error.log';
+    this.logLevel = Number(process.env.LOG_LEVEL);
+    this.maxFileSize = Number(process.env.MAX_FILE_SIZE);
+    this.pathToLogsFile = join(currentWorkingDir, this.logsDir, logsFile);
+    this.pathToLogsErrorFile = join(
+      currentWorkingDir,
+      this.logsDir,
+      logsErrorFile,
+    );
     this.createDir();
   }
 
   log(message: string, ...params: unknown[]): void {
-    this.writeToFile(message, params, LevelLog.log);
+    if (this.logLevel >= numericLevelLog.log) {
+      this.writeToFile(message, params, LevelLog.log);
+    }
   }
 
   fatal(message: string, ...params: unknown[]): void {
-    this.writeToFile(message, params, LevelLog.fatal);
+    if (this.logLevel >= numericLevelLog.fatal) {
+      this.writeToFile(message, params, LevelLog.fatal);
+    }
   }
 
   error(message: string, ...params: unknown[]): void {
-    this.writeToFile(message, params, LevelLog.error);
+    if (this.logLevel >= numericLevelLog.error) {
+      this.writeToFile(message, params, LevelLog.error);
+    }
   }
 
   warn(message: string, ...params: unknown[]): void {
-    this.writeToFile(message, params, LevelLog.warn);
+    if (this.logLevel >= numericLevelLog.warn) {
+      this.writeToFile(message, params, LevelLog.warn);
+    }
   }
 
   debug(message: string, ...params: unknown[]): void {
-    this.writeToFile(message, params, LevelLog.debug);
+    if (this.logLevel >= numericLevelLog.debug) {
+      this.writeToFile(message, params, LevelLog.debug);
+    }
   }
 
   verbose(message: string, ...params: unknown[]): void {
-    this.writeToFile(message, params, LevelLog.verbose);
+    if (this.logLevel >= numericLevelLog.verbose) {
+      this.writeToFile(message, params, LevelLog.verbose);
+    }
   }
 
   private createTime(): string {
     return new Date().toLocaleString('en-US', {
       hour12: true,
     });
+  }
+
+  private async checkFileSize(path: string): Promise<boolean> {
+    const size = (await stat(path)).size;
+    const sizeInBytes = Math.floor(size / 1024);
+    return sizeInBytes >= this.maxFileSize;
   }
 
   private async writeToFile(
@@ -57,16 +86,20 @@ export class CustomLogger implements LoggerService {
     } - ${this.createTime()}     ${level} [${params}] ${message}`;
 
     try {
-      await appendFile(
-        join(process.cwd(), this.logsDir, this.logsFile),
-        logMessage + '\n',
-      );
+      await appendFile(this.pathToLogsFile, logMessage + '\n');
+      const isRemoveLogFile = await this.checkFileSize(this.pathToLogsFile);
+      if (isRemoveLogFile) {
+        await rm(this.pathToLogsFile);
+      }
 
       if (level === LevelLog.error) {
-        await appendFile(
-          join(process.cwd(), this.logsDir, this.logsErrorFile),
-          logMessage + '\n',
+        await appendFile(this.pathToLogsErrorFile, logMessage + '\n');
+        const isRemoveLogErrorFile = await this.checkFileSize(
+          this.pathToLogsErrorFile,
         );
+        if (isRemoveLogErrorFile) {
+          await rm(this.pathToLogsErrorFile);
+        }
       }
     } catch (error) {
       console.log(error.message);
--- src/logger/types/index.ts
+++ src/logger/types/index.ts
@@ -7,4 +7,13 @@ export const LevelLog = {
   verbose: 'VERBOSE',
 } as const;
 
+export const numericLevelLog = {
+  fatal: 0,
+  error: 1,
+  log: 2,
+  warn: 3,
+  debug: 4,
+  verbose: 5,
+};
+
 export type TLevelLog = typeof LevelLog[keyof typeof LevelLog];
